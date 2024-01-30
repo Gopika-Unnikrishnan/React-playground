d3e0bb3164c348abceaf548e3cf00ba94bb3c186
ajayudayasanu <ajayudayasanu95@gmail.com>
2024-01-30 23:24:00 +0530

    Started with Task Tracker

--- src/Pages/TaskTracker/Tasktracker.jsx
+++ src/Pages/TaskTracker/Tasktracker.jsx
@@ -1,6 +1,30 @@
-const TaskTracker = () =>{
-    return(
-        <h1>Task</h1>
+import { useState, useEffect } from 'react'
+
+const TaskTracker = () => {
+    const [tasks,setTasks] = useState([])
+    const fetchTask = async () => {
+        const res = await fetch("http://localhost:5000/tasks")
+        const data = res.json()
+        return data
+    }
+    useEffect(() => {
+        const getTask = async () => {
+            const tasksFromServer = await fetchTask()
+            setTasks(tasksFromServer)
+        }
+        getTask()
+    }, [])
+    return (
+        <div>
+            <h1>Task</h1>
+            <ol>
+                {tasks.map(task=>(
+                    <li>
+                        {task.text}
+                    </li>
+                ))}
+            </ol>
+        </div>
     )
 }
 
